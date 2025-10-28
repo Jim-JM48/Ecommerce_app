9976b037312d154bf548474def819dabd88c2473
unknown <jimmyjim4821@gmail.com>
2025-10-28 23:15:48 +0300

    update layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
   {path: "product/:id",component: ProductComponent},
-  {path: "cart",component: CartComponent}
+  {path: "cart",component: CartComponent},
+  {path : '**' ,component : NotfoundComponent}
 ];
